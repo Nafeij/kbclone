30eac2a28d13408e3d714309613f951fbd1b6847
Wang Jiefan <wng.jiefan@gmail.com>
2023-02-07 22:16:53 +0800

    conversion to sass

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './game.css'
+// import './game.css'
+import './game.sass'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 
