acd1d35886ef6802d36dd10c777f9387dc96bae0
Wang Jiefan <wng.jiefan@gmail.com>
2023-02-11 16:23:46 +0800

    root changes

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 // import './game.css'
 import './game.sass'
-import App from './App'
+import App from './App.js'
 import reportWebVitals from './reportWebVitals'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
