6678084c008ca3ed74996d1ef94ce3617a4db260
Wang Jiefan <wng.jiefan@gmail.com>
2023-03-04 00:50:52 +0800

    Switched to HashRouter

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import reportWebVitals from './reportWebVitals'
 
 // import './game.css'
@@ -12,9 +12,9 @@ import './game.sass'
 const navTree = new NavTree()
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  <BrowserRouter basename='/kbclone'>
+  <HashRouter>
     <App tree={navTree}/>
-  </BrowserRouter>
+  </HashRouter> // Switch to BrowserRouter if deploying somewhere other than github pages
 )
 
 // If you want to start measuring performance in your app, pass a function
